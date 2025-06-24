cdfa6eff25ad6ce612c6c0d91a1bfb42039c9681
Rafibarides <rafibaridesmusic@gmail.com>
2025-06-24 14:56:07 -0400

    Updates

--- assets/vendor-ByThjEC3.js
+++ assets/vendor-ByThjEC3.js
@@ -1,4 +1,4 @@
-import{g as bn}from"./react-DJG_os-6.js";import{R as Ne}from"./framer-CpTVDtNv.js";/*!
+import{g as bn}from"./react-DJG_os-6.js";import{R as Ne}from"./framer-B4iRoS34.js";/*!
  * Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com
  * License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
  * Copyright 2024 Fonticons, Inc.
